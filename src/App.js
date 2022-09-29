5003a438264f8c8087df5d219c804b1e8e3d0fc9
sabrinamaral <sabrinamaral@hotmail.com>
2022-09-29 11:39:52 -0300

    dashboard, error, login page and router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Dashboard></Dashboard>
-      <Login />
-      <Error />
+      <Router>
+        <Switch>
+          <Route path="/" exact={true}>
+            <Dashboard />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route>
+            <Error path="*" />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
