cfb6d555faae655b3c559dd4ef046e6e11081585
sabrinamaral <sabrinamaral@hotmail.com>
2022-09-30 09:19:13 -0300

    pie chart

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
+import ReactFC from "react-fusioncharts";
+import FusionCharts from "fusioncharts";
+import Chart from "fusioncharts/fusioncharts.charts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
-const Pie3D = () => {
-  return <div>chart</div>;
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "Pie3D", // The chart type
+    width: "400", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        captio: "Languages",
+        theme: "fusion",
+        decimals: 0,
+        pieRadius: "45%",
+      },
+      // Chart Data
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Pie3D;
+export default ChartComponent;
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,38 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React from "react";
+import styled from "styled-components";
+import { useGlobalContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = useGlobalContext();
+  let languages = repos.reduce((acc, curr) => {
+    const { language } = curr;
+    if (!language) return acc;
+    if (!acc[language]) {
+      acc[language] = { label: language, value: 1 };
+    } else {
+      acc[language] = { ...acc[language], value: acc[language].value + 1 };
+    }
+    return acc;
+  }, {});
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  const chartData = [
+    { label: "Javascript", value: 64 },
+    { label: "HTML", value: 13 },
+    { label: "CSS", value: 23 },
+  ];
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <Pie3D data={languages} />
+        {/* <ExampleChart data={chartData} /> */}
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
