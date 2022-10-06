5f397ef161202e1c42f9b9c06411783da1cdfc9f
sabrinamaral <sabrinamaral@hotmail.com>
2022-10-06 14:44:57 -0300

    react router 6 | refact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <AuthWrapper>
         <Router>
-          <Switch>
-            <PrivateRoute path="/" exact={true}>
-              <Dashboard />
-            </PrivateRoute>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route>
-              <Error path="*" />
-            </Route>
-          </Switch>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
         </Router>
       </AuthWrapper>
     </div>
--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,17 +1,12 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-
-const PrivateRoute = ({ children, ...rest }) => {
+import { Navigate } from "react-router-dom";
+const PrivateRoute = ({ children }) => {
   const { isAuthenticated, user } = useAuth0();
   const isUser = isAuthenticated && user;
-  return (
-    <Route
-      {...rest}
-      render={() => {
-        return isUser ? children : <Redirect to="login" />;
-      }}
-    ></Route>
-  );
+  if (!isUser) {
+    return <Navigate to="/login" />;
+  }
+  return children;
 };
 export default PrivateRoute;
